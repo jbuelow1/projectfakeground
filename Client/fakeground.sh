7aafe4fa165b81b16cae0f8947ee2eed6b064128
jbuelow1 <jdbuelow@gmail.com>
2018-03-06 14:44:00 -0600

    Add files via upload

--- Client/fakeground.sh
+++ Client/fakeground.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
 json=$(curl https://jbuelow1.github.io/projectfakeground/Server/fakeground.json)
-masterswitch=$(echo $json | grep -Po '"masterswitch":.*?[^\\]",')
+prop="masterswitch"
+masterswitch=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop)
 
 echo "Starting..."
 if [[ $masterswitch == *"true"* ]]
 then
   echo "masterswitch is enabled!"
-  me_enabled=$(echo $json | grep -Po '"andrew_enabled":.*?[^\\]",')
+  prop="andrew_enabled"
+  me_enabled=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop)
   if [[ $me_enabled == *"true"* ]]
   then
     echo "This device is enabled!"
-    image_id=$(echo $json | grep -Po '"andrew_id":.*?[^\\]",' | cut -d '"' -f 4)
-    current_id=$(cat .current_id)
+    prop="andrew_id"
+    image_id=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop)
+    current_id=$(cat ~/.current_id)
     if [ "$image_id" != "$current_id" ]
     then
       echo "New image discovered!"
-      image_format=$(echo $json | grep -Po '"andrew_format":.*?[^\\]",' | cut -d '"' -f 4)
-      image_url=$(echo $json | grep -Po '"andrew_url":.*?[^\\]",' | cut -d '"' -f 4)
+      echo $image_id > ~/.current_id
+      prop="andrew_format"
+      image_format=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop | cut -d "|" -f 2)
+      prop="andrew_url"
+      image_url=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop | cut -d "|" -f 2)
       curl -o ${HOME}/.background.$image_format $image_url
-      sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '${HOME}/.background.$image_format'"
-      killall Dock
+      #sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '${HOME}/.background.$image_format'"
+      #killall Dock
+      echo $image_format
+      echo $image_url
       echo "Done."
     fi
   fi
