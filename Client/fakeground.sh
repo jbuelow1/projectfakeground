d88b26bb270a39c6be7dbc2aacbce7158a10f628
jbuelow1 <jdbuelow@gmail.com>
2018-03-07 16:02:08 -0600

    Update fakeground.sh

--- Client/fakeground.sh
+++ Client/fakeground.sh
@@ -8,21 +8,21 @@ echo "Starting..."
 if [[ $masterswitch == *"true"* ]]
 then
   echo "masterswitch is enabled!"
-  prop="andrew_enabled"
+  prop="test_enabled"
   me_enabled=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop)
   if [[ $me_enabled == *"true"* ]]
   then
     echo "This device is enabled!"
-    prop="andrew_id"
+    prop="test_id"
     image_id=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop)
     current_id=$(cat ~/.current_id)
     if [ "$image_id" != "$current_id" ]
     then
       echo "New image discovered!"
       echo $image_id > ~/.current_id
-      prop="andrew_format"
+      prop="test_format"
       image_format=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop | cut -d "|" -f 2)
-      prop="andrew_url"
+      prop="test_url"
       image_url=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop | cut -d "|" -f 2)
       curl -o ${HOME}/.background.$image_format $image_url
       sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '${HOME}/.background.$image_format'"
