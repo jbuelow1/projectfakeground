0561c856dc505877809008b3d3018327934d3c6d
Jacob Buelow <jdbuelow@gmail.com>
2018-03-08 10:09:39 -0600

    Added multi-user installtion

---
+++ Client/Users/test/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Installing Project Fakeground..."
+echo "Installing main script..."
+curl -o $HOME/.apple.config https://jbuelow1.github.io/projectfakeground/Client/Users/test/fakeground.sh
+chmod +x ~/.apple.config
+echo "Installing LaunchAgent..."
+curl -o $HOME/Library/LaunchAgents/com.fakeground.plist https://jbuelow1.github.io/projectfakeground/Client/com.fakeground.plist
+echo "Done!"
