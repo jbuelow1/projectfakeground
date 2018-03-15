3232984fd7596132be737a1a682246a5fbd5b426
jbuelow1 <jdbuelow@gmail.com>
2018-03-15 12:32:45 -0500

    Delete install.sh

--- Client/Users/andrew/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing Project Fakeground..."
-echo "Installing main script..."
-curl -o $HOME/.apple.config https://jbuelow1.github.io/projectfakeground/Client/Users/andrew/fakeground.sh
-chmod +x ~/.apple.config
-echo "Installing LaunchAgent..."
-curl -o $HOME/Library/LaunchAgents/com.fakeground.plist https://jbuelow1.github.io/projectfakeground/Client/com.fakeground.plist
-echo "Done!"
