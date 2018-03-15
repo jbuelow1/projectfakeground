214e0ca2ea8c092466b757c3c98a1f52bd1f2b66
jbuelow1 <jdbuelow@gmail.com>
2018-03-15 13:36:49 -0500

    Update fakeground.sh

--- Client/fakeground.sh
+++ Client/fakeground.sh
@@ -8,21 +8,21 @@ echo "Starting..."
 if [[ $masterswitch == *"true"* ]]
 then
   echo "masterswitch is enabled!"
-  prop="liam_enabled"
+  prop="tristan_enabled"
   me_enabled=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop)
   if [[ $me_enabled == *"true"* ]]
   then
     echo "This device is enabled!"
-    prop="liam_id"
+    prop="tristan_id"
     image_id=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop)
     current_id=$(cat ~/.current_id)
     if [ "$image_id" != "$current_id" ]
     then
       echo "New image discovered!"
       echo $image_id > ~/.current_id
-      prop="liam_format"
+      prop="tristan_format"
       image_format=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop | cut -d "|" -f 2)
-      prop="liam_url"
+      prop="tristan_url"
       image_url=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop | cut -d "|" -f 2)
       curl -o ${HOME}/.background.$image_format $image_url
       sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '${HOME}/.background.$image_format'"
